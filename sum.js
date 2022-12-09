9d0a9c6910571fbac50ad770c6920fc4303f2a68
Ray-hub928 <ray3111.a509@gmail.com>
2022-12-09 11:40:28 +0800

    修改sum.js內容

--- sum.js
+++ sum.js
@@ -1,5 +1,10 @@
 function sum(n) {
     // 回傳 1+2+3+4+...+n 的結果
+    let result = 0;
+    for (let i = 1; i <= n; i++) {
+      result += i;
+    }
+    return result;
   }
   
   console.log(sum(1)); // 1
